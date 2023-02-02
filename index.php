2dc1a4faf2d96a7338dbd17892f3db80107c5629
Mark Kevin Mayormita <michaelortizmayormita@Michaels-MacBook-Air.local>
2023-02-02 21:22:19 +0800

    dependencies setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <!-- js -->
+    <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/jquery-3.6.3.min.js"></script>
+    <script src="public/js/popper.min.js"></script>
+    <title>Remote Philippines</title>
+</head>
+<body class="container-fluid">
+    
+</body>
+</html>
